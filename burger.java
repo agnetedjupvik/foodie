0cb174fb507189c6615d8ea96c22fea6550027a4
Agnete Djupvik <agnetedjupvik@gmail.com>
2016-11-05 13:13:45 +0100

    Create burger.java

---
+++ burger.java
@@ -0,0 +1,21 @@
+I en blender:
+En stor gulrot
+En halv paprika
+En boks brune bønner/kidney bønner
+2dl havregryn
+En halv teskje Oregano
+En halv teskje Hvitløkspulver
+En halv teskje Persille
+En klype salt
+Miks, men ikke for mye. Skal være tekstur.
+
+
+Del opp i 5-6 kaker.
+Stek i olje ca 3min på hver side
+Sett de på en stekeplate med aliminiumsfolie og stek i 10min på 175C
+
+
+Server med hamburgerbrød, stekte poteter og tilbehør av ditt ønske :)
+
+
+Bilde kommer senere (må bare lage de hehe)
