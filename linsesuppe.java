37f1cc65bbaadcabee0d930a62728b11e8c7cf5a
Agnete Djupvik <agnetedjupvik@gmail.com>
2017-02-12 14:05:19 +0100

    Create linsesuppe.java

---
+++ linsesuppe.java
@@ -0,0 +1,22 @@
+Rød linsesuppe
+
+1 liten gul løk, hakket
+1 hvitløkfedd, finhakket
+1 rød chili, finhakket
+1 ts paprikapulver
+6 dl vann
+2 grønnsaksbuljongterninger 
+1 boks hakka tomater
+1 boks røde linser drenert (200g)
+1 boks kokosmelk
+1 ts sukker
+Salt og pepper til smak
+
+Kutt opp løk, brun med litt olje
+Tilsett hvitløk og chili
+Kok opp vann, tilsett grønnsaksbuljong
+Tilsett linsene, kok 20min
+Tilsett hakka tomater, kokosmelk, paprikapulver, sukker og løkblandingen
+Kok 5min
+Miks med stavmikser
+Smak til med salt og pepper
