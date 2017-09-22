7df8efd158a69e18f668f8043f851d5ed03d144f
Agnete Djupvik <agnetedjupvik@gmail.com>
2017-09-22 11:41:14 +0200

    Create blomkålsoup.java

---
+++ blomkålsoup.java
@@ -0,0 +1,21 @@
+Blomkålsuppe
+
+Hva sier mer høst enn en god, varm suppe? I tillegg til at det er skikkelig godt, koster det ikke skjorta heller!
+
+Til 3-4 porsjoner trenger du omtrent:
+
+- 1 stort blomkålhode, i buketter
+- 1 liten løk, finhakket
+- 8 dl kyllingkraft
+- 2 dl melk
+- 150 gram bacon, strimlet
+- salt og pepper
+- frisk persille, finhakket
+
+1. Rens og vask blomkålen og del den opp i jevnstore buketter. Skjær også stilken opp i biter. Hold av noen små buketter som skal benyttes som garnityr.
+
+2. Kok opp kraft og melk og tilsett blomkål og løk, og la det småkoke til blomkålen er mør. Beregn ca. 15 minutter. Kjør suppen jevn med en stavmikser eller i en foodprosessor. Gi suppen et lett oppkok, og smak til med salt og pepper.
+
+3. Mens blomkålen koker sprøsteker du baconet i en tørr stekepanne. Legg baconet til side. Ha resten av blomkålbukettene i pannen og tilsett om nødvendig litt smør eller olje. La blomkålen frese på middels varme til den tar litt farge og er så vidt mør.
+
+4. Mot slutten av steketiden tilsettes det sprøstekte baconet. Smak til med litt salt, om nødvendig. (Baconet kan eventuelt erstattes med sprøstekt spekeskinke eller chorizo.)
